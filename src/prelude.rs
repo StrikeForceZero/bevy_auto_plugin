f1692f263cdf4f4aabe5c03ee2758e604aa88e45
bstriker <strikeforcezero@gmail.com>
2025-06-29 13:58:31 -0400

    refactor: merge nightly proc macros into proc macros crate

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,37 +1,37 @@
 pub mod module {
     pub mod prelude {
         #[doc(inline)]
-        pub use bevy_auto_plugin_proc_macros::auto_plugin;
+        pub use bevy_auto_plugin_proc_macros::module_auto_plugin as auto_plugin;
         #[doc(inline)]
-        pub use bevy_auto_plugin_proc_macros::auto_add_event;
+        pub use bevy_auto_plugin_proc_macros::module_auto_add_event as auto_add_event;
         #[doc(inline)]
-        pub use bevy_auto_plugin_proc_macros::auto_init_resource;
+        pub use bevy_auto_plugin_proc_macros::module_auto_init_resource as auto_init_resource;
         #[doc(inline)]
-        pub use bevy_auto_plugin_proc_macros::auto_init_state;
+        pub use bevy_auto_plugin_proc_macros::module_auto_init_state as auto_init_state;
         #[doc(inline)]
-        pub use bevy_auto_plugin_proc_macros::auto_name;
+        pub use bevy_auto_plugin_proc_macros::module_auto_name as auto_name;
         #[doc(inline)]
-        pub use bevy_auto_plugin_proc_macros::auto_register_type;
+        pub use bevy_auto_plugin_proc_macros::module_auto_register_type as auto_register_type;
         #[doc(inline)]
-        pub use bevy_auto_plugin_proc_macros::auto_register_state_type;
+        pub use bevy_auto_plugin_proc_macros::module_auto_register_state_type as auto_register_state_type;
     }
 }
 
 pub mod inline {
     pub mod prelude {
         #[doc(inline)]
-        pub use bevy_auto_plugin_nightly_proc_macros::auto_plugin;
+        pub use bevy_auto_plugin_proc_macros::inline_auto_plugin as auto_plugin;
         #[doc(inline)]
-        pub use bevy_auto_plugin_nightly_proc_macros::auto_add_event;
+        pub use bevy_auto_plugin_proc_macros::inline_auto_add_event as auto_add_event;
         #[doc(inline)]
-        pub use bevy_auto_plugin_nightly_proc_macros::auto_init_resource;
+        pub use bevy_auto_plugin_proc_macros::inline_auto_init_resource as auto_init_resource;
         #[doc(inline)]
-        pub use bevy_auto_plugin_nightly_proc_macros::auto_init_state;
+        pub use bevy_auto_plugin_proc_macros::inline_auto_init_state as auto_init_state;
         #[doc(inline)]
-        pub use bevy_auto_plugin_nightly_proc_macros::auto_name;
+        pub use bevy_auto_plugin_proc_macros::inline_auto_name as auto_name;
         #[doc(inline)]
-        pub use bevy_auto_plugin_nightly_proc_macros::auto_register_type;
+        pub use bevy_auto_plugin_proc_macros::inline_auto_register_type as auto_register_type;
         #[doc(inline)]
-        pub use bevy_auto_plugin_nightly_proc_macros::auto_register_state_type;
+        pub use bevy_auto_plugin_proc_macros::inline_auto_register_state_type as auto_register_state_type;
     }
 }
