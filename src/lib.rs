74772ef7738fe0467b5cf29f1e1ee2bab2516b97
bstriker <strikeforcezero@gmail.com>
2025-06-29 11:57:27 -0400

    chore: deprecate auto_plugin and auto_plugin_module exports in favor of prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod prelude;
 
+#[deprecated(since = "0.3.0", note = "use bevy_auto_plugin::prelude::inline instead")]
 #[doc(inline)]
 pub use bevy_auto_plugin_nightly_proc_macros as auto_plugin;
+#[deprecated(since = "0.3.0", note = "use bevy_auto_plugin::prelude::module instead")]
 #[doc(inline)]
 pub use bevy_auto_plugin_proc_macros as auto_plugin_module;
