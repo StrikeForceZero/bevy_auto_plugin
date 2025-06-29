5f30288b84b26326132ecabead7e57d366189a08
bstriker <strikeforcezero@gmail.com>
2025-06-29 14:00:39 -0400

    chore: remove bevy_auto_plugin_nightly_proc_macros

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub use prelude::*;
 
 #[deprecated(since = "0.3.0", note = "use bevy_auto_plugin::inline::prelude instead")]
 #[doc(inline)]
-pub use bevy_auto_plugin_nightly_proc_macros as auto_plugin;
+pub use crate::inline::prelude as auto_plugin;
 #[deprecated(since = "0.3.0", note = "use bevy_auto_plugin::module::prelude instead")]
 #[doc(inline)]
-pub use bevy_auto_plugin_proc_macros as auto_plugin_module;
+pub use crate::module::prelude as auto_plugin_module;
