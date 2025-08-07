a34b6059b1c4d2d3f818c6684ec73dcbce3a9007
bstriker <strikeforcezero@gmail.com>
2025-08-07 11:19:08 -0400

    refactor: remove deprecated import aliases

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,2 @@
 mod prelude;
 pub use prelude::*;
-
-#[cfg(feature = "mode_flat_file")]
-#[deprecated(
-    since = "0.3.0",
-    note = "use bevy_auto_plugin::flat_file::prelude instead"
-)]
-#[doc(inline)]
-pub use crate::flat_file::prelude as auto_plugin;
-
-#[cfg(feature = "mode_module")]
-#[deprecated(
-    since = "0.3.0",
-    note = "use bevy_auto_plugin::module::prelude instead"
-)]
-#[doc(inline)]
-pub use crate::module::prelude as auto_plugin_module;
