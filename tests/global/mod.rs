28a92fd4a3b1380dde61563707097ce2cd23be5f
bstriker <strikeforcezero@gmail.com>
2025-08-14 16:54:37 -0400

    chore(tests): add tests for global mode default app param

--- tests/global/mod.rs
+++ tests/global/mod.rs
@@ -1,5 +1,7 @@
 mod auto_plugin;
 mod auto_plugin_with_generics;
 mod bare_fn;
+mod bare_fn_default_app_param;
 mod self_impl_plugin;
+mod self_impl_plugin_default_app_param;
 mod self_impl_plugin_with_generics;
---
+++ tests/global/self_impl_plugin_default_app_param.rs
@@ -0,0 +1,38 @@
+use bevy::prelude::*;
+use bevy_auto_plugin::modes::global::prelude::*;
+use bevy_auto_plugin_proc_macros::{global_auto_init_resource, global_auto_insert_resource};
+
+#[derive(AutoPlugin)]
+pub struct MyPlugin;
+
+#[derive(Resource, Default, PartialEq, Debug)]
+#[global_auto_init_resource(plugin = MyPlugin)]
+#[global_auto_insert_resource(plugin = MyPlugin, resource(MyResourceAuto(1)))]
+pub struct MyResourceAuto(usize);
+
+#[derive(Resource, Default, PartialEq, Debug)]
+#[global_auto_init_resource(plugin = MyPlugin)]
+pub struct MyResourceBuild(usize);
+
+impl Plugin for MyPlugin {
+    #[global_auto_plugin]
+    fn build(&self, app: &mut App) {
+        app.insert_resource(MyResourceBuild(1));
+    }
+}
+
+#[internal_test_proc_macro::xtest]
+fn test() {
+    let mut app = App::new();
+    app.add_plugins(MyPlugin);
+    assert_eq!(
+        app.world().get_resource::<MyResourceBuild>(),
+        Some(&MyResourceBuild(1)),
+        "build function was not called"
+    );
+    assert_eq!(
+        app.world().get_resource::<MyResourceAuto>(),
+        Some(&MyResourceAuto(1)),
+        "auto plugin failed"
+    );
+}
