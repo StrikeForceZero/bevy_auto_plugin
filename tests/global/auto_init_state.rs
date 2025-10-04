5bf6e8dbc96e5df1a65440e16687653ccbd8a7b2
bstriker <strikeforcezero@gmail.com>
2025-10-04 10:50:32 -0400

    chore(tests): restore tests from other modes into global

---
+++ tests/global/auto_init_state.rs
@@ -0,0 +1,53 @@
+use bevy_app::prelude::*;
+use bevy_auto_plugin::modes::global::prelude::*;
+use bevy_state::app::StatesPlugin;
+use bevy_state::prelude::*;
+
+#[derive(AutoPlugin)]
+#[auto_plugin(impl_plugin_trait)]
+struct TestPlugin;
+
+#[auto_init_state(plugin = TestPlugin)]
+#[derive(States, Debug, Copy, Clone, Default, PartialEq, Eq, Hash)]
+enum Test {
+    #[default]
+    A,
+    #[allow(dead_code)]
+    B,
+}
+
+#[auto_init_state(plugin = TestPlugin)]
+#[derive(SubStates, Debug, Copy, Clone, Default, PartialEq, Eq, Hash)]
+#[source(Test = Test::B)]
+enum InnerTest {
+    #[default]
+    A,
+}
+
+fn app() -> App {
+    let mut app = internal_test_util::create_minimal_app();
+    app.add_plugins(StatesPlugin);
+    app.add_plugins(TestPlugin);
+    app
+}
+
+#[internal_test_proc_macro::xtest]
+fn test_auto_init_state() {
+    let app = app();
+    assert!(
+        app.world().get_resource::<State<Test>>().is_some(),
+        "did not auto init state"
+    );
+    assert!(
+        app.world().get_resource::<NextState<Test>>().is_some(),
+        "did not auto init state"
+    );
+    assert!(
+        app.world().get_resource::<State<InnerTest>>().is_some(),
+        "did not auto init state"
+    );
+    assert!(
+        app.world().get_resource::<NextState<InnerTest>>().is_some(),
+        "did not auto init state"
+    );
+}
---
+++ tests/global/auto_plugin_default_param.rs
@@ -0,0 +1,34 @@
+use bevy_app::prelude::*;
+use bevy_auto_plugin::modes::global::prelude::*;
+use bevy_ecs::prelude::*;
+
+#[derive(AutoPlugin)]
+struct TestPlugin;
+
+impl Plugin for TestPlugin {
+    #[auto_plugin]
+    fn build(&self, app: &mut App) {
+        app.init_resource::<Test>();
+    }
+}
+
+#[derive(Resource, Debug, PartialEq)]
+struct Test(usize);
+
+impl Default for Test {
+    fn default() -> Self {
+        Self(1)
+    }
+}
+
+fn app() -> App {
+    let mut app = internal_test_util::create_minimal_app();
+    app.add_plugins(TestPlugin);
+    app
+}
+
+#[internal_test_proc_macro::xtest]
+fn test_auto_plugin_param() {
+    let app = app();
+    assert_eq!(app.world().get_resource::<Test>(), Some(&Test(1)));
+}
