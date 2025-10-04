afe25975988240e203207ee1718e56908819d976
bstriker <strikeforcezero@gmail.com>
2025-10-04 10:25:02 -0400

    refactor: remove flat_file and global modes

--- src/modes/mod.rs
+++ src/modes/mod.rs
@@ -1,20 +1,2 @@
-#[cfg(feature = "mode_flat_file")]
-#[cfg_attr(
-    not(feature = "ignore_flat_file_or_module_deprecation"),
-    deprecated(
-        since = "0.5.0",
-        note = "See https://github.com/StrikeForceZero/bevy_auto_plugin/issues/19"
-    )
-)]
-pub mod flat_file;
 #[cfg(feature = "mode_global")]
 pub mod global;
-#[cfg(feature = "mode_module")]
-#[cfg_attr(
-    not(feature = "ignore_flat_file_or_module_deprecation"),
-    deprecated(
-        since = "0.5.0",
-        note = "See https://github.com/StrikeForceZero/bevy_auto_plugin/issues/19"
-    )
-)]
-pub mod module;
--- tests/mod.rs
+++ tests/mod.rs
@@ -1,6 +1,2 @@
-#[cfg(feature = "mode_flat_file")]
-mod flat_file;
 #[cfg(feature = "mode_global")]
 mod global;
-#[cfg(feature = "mode_module")]
-mod module;
