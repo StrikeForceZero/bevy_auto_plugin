f3d1c2defc27478cd747d626fe6a245661e0b4ee
bstriker <strikeforcezero@gmail.com>
2025-10-10 16:38:38 -0400

    refactor: remove unused `span` module from diagnostics

--- crates/bevy_auto_plugin_shared/src/syntax/diagnostic/span.rs
+++
@@ -1,8 +0,0 @@
-use proc_macro2::Span;
-
-pub fn get_or_create_span_from_result<T>(e: &syn::Result<T>) -> Span {
-    match e {
-        Ok(_) => Span::call_site(),
-        Err(e) => e.span(),
-    }
-}
