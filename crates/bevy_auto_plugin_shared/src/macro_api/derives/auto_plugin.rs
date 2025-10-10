83cf05acd9e6d12001e1132f5bc8477b6ac207a9
bstriker <strikeforcezero@gmail.com>
2025-10-10 10:16:00 -0400

    refactor: add `#[allow(dead_code)]` to unused fields in `AutoPluginDeriveArgs`

--- crates/bevy_auto_plugin_shared/src/macro_api/derives/auto_plugin.rs
+++ crates/bevy_auto_plugin_shared/src/macro_api/derives/auto_plugin.rs
@@ -8,9 +8,12 @@ use syn::{Attribute, Generics, Visibility};
 #[darling(attributes(auto_plugin), forward_attrs, supports(struct_any, enum_any))]
 pub struct AutoPluginDeriveArgs {
     pub ident: Ident,
+    #[allow(dead_code)]
     pub vis: Visibility,
     pub generics: Generics,
+    #[allow(dead_code)]
     pub data: darling::ast::Data<VariantData, FieldData>,
+    #[allow(dead_code)]
     pub attrs: Vec<Attribute>,
     #[darling(flatten)]
     pub auto_plugin: AutoPluginStructOrEnumAttributeArgs,
