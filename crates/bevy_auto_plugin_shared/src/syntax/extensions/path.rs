2208ef2388e4cad163588ab698bdcf33238cb400
bstriker <strikeforcezero@gmail.com>
2025-10-16 10:06:10 -0400

    feat: add `is_similar_path_or_ident` method to `PathExt` and implement tests

--- crates/bevy_auto_plugin_shared/src/syntax/extensions/path.rs
+++ crates/bevy_auto_plugin_shared/src/syntax/extensions/path.rs
@@ -9,6 +9,8 @@ pub trait PathExt {
     fn generic_count(&self) -> syn::Result<usize> {
         Ok(self.generics()?.len())
     }
+
+    fn is_similar_path_or_ident(&self, other: &Self) -> bool;
 }
 
 impl PathExt for Path {
@@ -18,5 +20,79 @@ impl PathExt for Path {
 
     fn generics(&self) -> syn::Result<TypeList> {
         concrete_path::generics_from_path(self)
+    }
+
+    fn is_similar_path_or_ident(&self, other: &Self) -> bool {
+        is_similar_path_or_ident(self, other)
+    }
+}
+
+/// Compares paths from right to left. Only returns false if a segment is different, not missing.
+///
+/// Examples:
+/// `::some::path == some::path == ::path == path`
+/// `::some::other::path != some::path`
+fn is_similar_path_or_ident(a: &Path, b: &Path) -> bool {
+    for (a, b) in a.segments.iter().rev().zip(b.segments.iter().rev()) {
+        if a.ident != b.ident || a.arguments != b.arguments {
+            return false;
+        }
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use internal_test_proc_macro::xtest;
+    use syn::parse_quote;
+
+    mod is_similar_path_or_ident {
+        use super::*;
+        #[xtest]
+        #[rustfmt::skip]
+        fn test_is_true() {
+            let a = parse_quote! { ::some::path };
+            assert!(is_similar_path_or_ident(&a, &parse_quote! { some::path }));
+            assert!(is_similar_path_or_ident(&a, &parse_quote! { ::path }));
+            assert!(is_similar_path_or_ident(&a, &parse_quote! { path }));
+        }
+
+        #[xtest]
+        #[rustfmt::skip]
+        fn test_is_false() {
+            let a = parse_quote! { ::some::path };    
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { ::some::other::path }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { ::other::path }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { other::path }));
+        }
+
+        // these are not valid paths, but we want to make sure they are handled correctly
+        #[xtest]
+        #[rustfmt::skip]
+        fn test_with_arguments_is_true() {
+            let a = parse_quote! { ::some<T>::path<T> };    
+            assert!(is_similar_path_or_ident(&a, &parse_quote! { some<T>::path<T> }));
+            assert!(is_similar_path_or_ident(&a, &parse_quote! { ::path<T> }));
+            assert!(is_similar_path_or_ident(&a, &parse_quote! { path<T> }));
+        }
+
+        #[xtest]
+        #[rustfmt::skip]
+        fn test_with_arguments_is_false() {
+            let a = parse_quote! { ::some<T>::path<T> };    
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { some<T>::path<F> }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { some<A>::path<T> }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { some::path<T> }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { some::path<F> }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { some<T>::path }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { some<F>::path }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { some::path }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { ::path<F> }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { ::path }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { path<F> }));
+            assert!(!is_similar_path_or_ident(&a, &parse_quote! { path }));
+        }
     }
 }
