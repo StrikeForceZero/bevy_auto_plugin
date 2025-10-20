bc6ec12b2d81588fc1d84939d56350e039048f5b
bstriker <strikeforcezero@gmail.com>
2025-10-20 16:54:43 -0400

    feat: implement `ToTokens` for `Q` with various `ItemAttribute` compositions on action macros

--- crates/bevy_auto_plugin_shared/src/macro_api/q.rs
+++ crates/bevy_auto_plugin_shared/src/macro_api/q.rs
@@ -1,9 +1,11 @@
 use crate::macro_api::attributes::ItemAttribute;
-use crate::macro_api::attributes::prelude::AddSystemArgs;
+use crate::macro_api::attributes::prelude::*;
 use crate::macro_api::composed::Composed;
 use crate::macro_api::context::Context;
 use crate::macro_api::input_item::InputItem;
+use crate::macro_api::mixins::generics::none::WithNoGenerics;
 use crate::macro_api::mixins::generics::with_many::WithZeroOrManyGenerics;
+use crate::macro_api::mixins::generics::with_single::WithZeroOrOneGenerics;
 use crate::macro_api::mixins::with_plugin::WithPlugin;
 use proc_macro2::TokenStream;
 use quote::{ToTokens, quote};
@@ -23,6 +25,122 @@ impl ToTokens
         for concrete_path in self.args.concrete_paths() {
             tokens.extend(quote! { |app| {
                 app.add_system(#schedule, #concrete_path);
+            }});
+        }
+    }
+}
+
+impl ToTokens
+    for Q<'_, ItemAttribute<Composed<AddMessageArgs, WithPlugin, WithZeroOrManyGenerics>>>
+{
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        for concrete_path in self.args.concrete_paths() {
+            tokens.extend(quote! { |app| {
+                app.add_message::<#concrete_path>();
+            }});
+        }
+    }
+}
+
+impl ToTokens
+    for Q<'_, ItemAttribute<Composed<AddObserverArgs, WithPlugin, WithZeroOrManyGenerics>>>
+{
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        for concrete_path in self.args.concrete_paths() {
+            tokens.extend(quote! { |app| {
+                app.add_observer::<#concrete_path>();
+            }});
+        }
+    }
+}
+
+impl ToTokens
+    for Q<'_, ItemAttribute<Composed<AddPluginArgs, WithPlugin, WithZeroOrManyGenerics>>>
+{
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        for concrete_path in self.args.concrete_paths() {
+            tokens.extend(quote! { |app| {
+                app.add_plugin::<#concrete_path>();
+            }});
+        }
+    }
+}
+
+impl ToTokens
+    for Q<'_, ItemAttribute<Composed<InitResourceArgs, WithPlugin, WithZeroOrManyGenerics>>>
+{
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        for concrete_path in self.args.concrete_paths() {
+            tokens.extend(quote! { |app| {
+                app.init_resource::<#concrete_path>();
+            }});
+        }
+    }
+}
+
+impl ToTokens for Q<'_, ItemAttribute<Composed<InitStateArgs, WithPlugin, WithNoGenerics>>> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        for concrete_path in self.args.concrete_paths() {
+            tokens.extend(quote! { |app| {
+                app.init_state::<#concrete_path>();
+            }});
+        }
+    }
+}
+
+impl ToTokens for Q<'_, ItemAttribute<Composed<InitSubStateArgs, WithPlugin, WithNoGenerics>>> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        for concrete_path in self.args.concrete_paths() {
+            tokens.extend(quote! { |app| {
+                app.init_sub_state::<#concrete_path>();
+            }});
+        }
+    }
+}
+
+impl ToTokens
+    for Q<'_, ItemAttribute<Composed<InsertResourceArgs, WithPlugin, WithZeroOrOneGenerics>>>
+{
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        for concrete_path in self.args.concrete_paths() {
+            tokens.extend(quote! { |app| {
+                app.insert_resource(#concrete_path::default());
+            }});
+        }
+    }
+}
+
+impl ToTokens
+    for Q<'_, ItemAttribute<Composed<RegisterStateTypeArgs, WithPlugin, WithZeroOrManyGenerics>>>
+{
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        for concrete_path in self.args.concrete_paths() {
+            tokens.extend(quote! { |app| {
+                app.register_state_type::<#concrete_path>();
+            }});
+        }
+    }
+}
+
+impl ToTokens
+    for Q<'_, ItemAttribute<Composed<RegisterTypeArgs, WithPlugin, WithZeroOrManyGenerics>>>
+{
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        for concrete_path in self.args.concrete_paths() {
+            tokens.extend(quote! { |app| {
+                app.register_type::<#concrete_path>();
+            }});
+        }
+    }
+}
+
+impl ToTokens
+    for Q<'_, ItemAttribute<Composed<RunOnBuildArgs, WithPlugin, WithZeroOrManyGenerics>>>
+{
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        for concrete_path in self.args.concrete_paths() {
+            tokens.extend(quote! { |app| {
+                #concrete_path(app);
             }});
         }
     }
