be55ee96e40af71d0eade6f44d7496f67a3f47f6
bstriker <strikeforcezero@gmail.com>
2025-10-24 20:16:33 -0400

    refactor: remove unused `attribute` module and related trait

--- crates/bevy_auto_plugin_shared/src/__private/attribute.rs
+++
@@ -1,6 +0,0 @@
-use crate::codegen::ExpandAttrs;
-use crate::syntax::validated::non_empty_path::NonEmptyPath;
-
-pub trait RewriteAttribute {
-    fn expand_attrs(&self, plugin: &NonEmptyPath) -> ExpandAttrs;
-}
