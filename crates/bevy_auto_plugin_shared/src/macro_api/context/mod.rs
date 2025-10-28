259a269d8896dec9d0e48aa0cf89f7b6830a8ba2
bstriker <strikeforcezero@gmail.com>
2025-10-28 12:35:13 -0400

    chore(docs): add TODO comment in `Context` for alias resolution in proc macros

--- crates/bevy_auto_plugin_shared/src/macro_api/context/mod.rs
+++ crates/bevy_auto_plugin_shared/src/macro_api/context/mod.rs
@@ -2,6 +2,7 @@ use macro_paths::MacroPaths;
 
 mod macro_paths;
 
+// TODO: pass from proc macros and resolve any aliases - instead of defaulting
 #[derive(Debug, Clone, Default, PartialEq, Hash)]
 pub struct Context {
     pub macros: MacroPaths,
