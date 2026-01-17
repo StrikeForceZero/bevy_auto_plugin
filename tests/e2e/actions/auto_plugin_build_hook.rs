3264adf22de2c065530564e18f95b0898caf0ae4
bstriker <strikeforcezero@gmail.com>
2026-01-17 16:32:47 -0500

    refactor: make `AutoPluginBuildHook` trait method instance-based and update macro to handle struct instances

--- tests/e2e/actions/auto_plugin_build_hook.rs
+++ tests/e2e/actions/auto_plugin_build_hook.rs
@@ -2,38 +2,54 @@ use bevy_app::prelude::*;
 use bevy_auto_plugin::prelude::*;
 use bevy_ecs::prelude::*;
 use internal_test_proc_macro::xtest;
-use std::any::TypeId;
+use std::{
+    any::TypeId,
+    collections::{
+        HashMap,
+        HashSet,
+    },
+};
 
 #[derive(AutoPlugin)]
 #[auto_plugin(impl_plugin_trait)]
 struct TestPlugin;
 
 #[derive(Resource, Debug, Default, PartialEq)]
-struct Counter(Vec<String>);
+struct Counter(HashMap<&'static str, HashMap<TypeId, HashSet<&'static str>>>);
 
 struct MyCustomHookA;
 
 impl<T: Component + 'static> AutoPluginBuildHook<T> for MyCustomHookA {
-    fn on_build(app: &mut App) {
-        app.world_mut().resource_mut::<Counter>().0.push(format!("A {:?}", TypeId::of::<T>()))
+    fn on_build(&self, app: &mut App) {
+        app.world_mut()
+            .resource_mut::<Counter>()
+            .0
+            .entry("A")
+            .or_default()
+            .entry(TypeId::of::<T>())
+            .or_default()
+            .insert(std::any::type_name::<T>());
     }
 }
 
-struct MyCustomHookB;
+struct MyCustomHookB(&'static str);
 
 impl<T: Component + 'static> AutoPluginBuildHook<T> for MyCustomHookB {
-    fn on_build(app: &mut App) {
-        app.world_mut().resource_mut::<Counter>().0.push(format!("B {:?}", TypeId::of::<T>()))
+    fn on_build(&self, app: &mut App) {
+        let mut counter = app.world_mut().resource_mut::<Counter>();
+        let set = counter.0.entry("B").or_default().entry(TypeId::of::<T>()).or_default();
+        set.insert(std::any::type_name::<T>());
+        set.insert(self.0);
     }
 }
 
 #[derive(Component, Debug)]
 #[auto_plugin_build_hook(plugin = TestPlugin, hook = MyCustomHookA)]
-#[auto_plugin_build_hook(plugin = TestPlugin, hook = MyCustomHookB)]
+#[auto_plugin_build_hook(plugin = TestPlugin, hook = MyCustomHookB("foo"))]
 struct TestA;
 
 #[derive(Component, Debug)]
-#[auto_plugin_build_hook(plugin = TestPlugin, hook = MyCustomHookB)]
+#[auto_plugin_build_hook(plugin = TestPlugin, hook = MyCustomHookB("bar"))]
 #[auto_plugin_build_hook(plugin = TestPlugin, hook = MyCustomHookA)]
 struct TestB;
 
@@ -47,21 +63,25 @@ fn app() -> App {
 #[xtest]
 fn test_auto_plugin_hook() {
     let app = app();
-    fn hook_a(test: TypeId) -> String {
-        format!("A {:?}", test)
-    }
-    fn hook_b(test: TypeId) -> String {
-        format!("B {:?}", test)
-    }
-    let test_a = TypeId::of::<TestA>();
-    let test_b = TypeId::of::<TestB>();
+    let counter = app.world().get_resource::<Counter>().expect("counter resource missing");
+
+    let hook_a_map = counter.0.get("A").expect("Hook A entry missing");
     assert_eq!(
-        app.world().get_resource::<Counter>(),
-        Some(&Counter(vec![
-            hook_a(test_a),
-            hook_a(test_b),
-            hook_b(test_b),
-            hook_b(test_a),
-        ]))
+        hook_a_map.get(&TypeId::of::<TestA>()).expect("Hook A - TestA entry missing"),
+        &HashSet::from([std::any::type_name::<TestA>()])
+    );
+    assert_eq!(
+        hook_a_map.get(&TypeId::of::<TestB>()).expect("Hook A - TestB entry missing"),
+        &HashSet::from([std::any::type_name::<TestB>()])
+    );
+
+    let hook_b_map = counter.0.get("B").expect("Hook B entry missing");
+    assert_eq!(
+        hook_b_map.get(&TypeId::of::<TestA>()).expect("Hook B - TestA entry missing"),
+        &HashSet::from([std::any::type_name::<TestA>(), "foo"])
+    );
+    assert_eq!(
+        hook_b_map.get(&TypeId::of::<TestB>()).expect("Hook B - TestB entry missing"),
+        &HashSet::from([std::any::type_name::<TestB>(), "bar"])
     );
 }
