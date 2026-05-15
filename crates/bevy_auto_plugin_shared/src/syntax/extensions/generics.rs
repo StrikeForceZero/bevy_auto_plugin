5652664dd11752dfa2c1888e880e58869063a26c
bstriker <strikeforcezero@gmail.com>
2026-05-14 20:10:27 -0400

    chore(format): apply rustfmt

--- crates/bevy_auto_plugin_shared/src/syntax/extensions/generics.rs
+++ crates/bevy_auto_plugin_shared/src/syntax/extensions/generics.rs
@@ -43,10 +43,9 @@ pub fn inject_send_sync_static(generics: &mut syn::Generics) {
                 }) => {
                     // e.g. ?Sized — ignore
                 }
-                TypeParamBound::Lifetime(lt)
-                    if lt == &Lifetime::new("'static", lt.apostrophe) => {
-                        has_static = true;
-                    }
+                TypeParamBound::Lifetime(lt) if lt == &Lifetime::new("'static", lt.apostrophe) => {
+                    has_static = true;
+                }
                 _ => {}
             }
         }
